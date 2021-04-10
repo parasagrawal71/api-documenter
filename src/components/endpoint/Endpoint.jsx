2f16578b1cc215a811095e51cab8f6a95038b794
Paras Agrawal <pagrawal@nference.net>
2021-04-10 23:29:58 +0530

    Initial Setup

---
+++ src/components/endpoint/Endpoint.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+// IMPORT USER-DEFINED COMPONENTS HERE
+
+// IMPORT ASSESTS HERE
+import appStyles from './Endpoint.module.scss';
+
+const Endpoint = () => {
+  return <section></section>;
+};
+
+export default Endpoint;
---
+++ src/pages/app/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css";
+// import Endpoint from ';
+
+function App() {
+  return <main>Hi</main>;
+}
+
+export default App;
