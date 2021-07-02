9299146327977adfef7887d1a7b11f018ae0402a
Paras Agrawal <pagrawal@nference.net>
2021-07-02 15:23:17 +0530

    Move env dropdown from main header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,69 +1,18 @@
 import React, { useState, useEffect } from "react";
-import { ClickAwayListener } from "@material-ui/core";
 import { Link } from "react-router-dom";
-import { ListAlt as ListAltIcon, ExitToApp as LogoutIcon } from "@material-ui/icons";
+import { ExitToApp as LogoutIcon } from "@material-ui/icons";
 import cx from "classnames";
-import { toast } from "react-toastify";
 
 // IMPORT USER-DEFINED COMPONENTS HERE
-import { ThemeAutocomplete, ThemeTextField } from "utils/commonStyles/StyledComponents";
-import EnvPopoverComponent from "components/envPopover/EnvPopover";
 import { clearSession } from "utils/cookie";
 import useGlobal from "redux/globalHook";
-import apiService from "apis/apiService";
-import { environment } from "apis/urls";
-import { getUrlParams } from "utils/functions";
 
 // IMPORT ASSETS HERE
 import appStyles from "./Header.module.scss";
 
 const Header = (props) => {
-  // PROPS HERE
-  const { environments, setEnvironments, selectedEnv, setSelectedEnv } = props;
-
-  // VARIABLES HERE
-  const serviceMID = getUrlParams()?.serviceMID;
-
   // HOOKS HERE
-  const [openEnvPopover, setOpenEnvPopover] = useState(null);
-  const [selectedEnvOldData, setSelectedEnvOldData] = useState(null);
   const [globalState] = useGlobal();
-
-  const createEnvironment = async (reqBody) => {
-    const response = await apiService(environment().post, { ...reqBody, serviceMID });
-    if (response?.success) {
-      // setEnvironments(); response?.data
-    }
-  };
-
-  const editEnvironment = async (updatedEnvironment) => {
-    const response = await apiService(environment(updatedEnvironment?._id).put, updatedEnvironment);
-    if (response?.success) {
-      setSelectedEnvOldData({});
-    } else {
-      setSelectedEnv(selectedEnvOldData);
-      toast.error("Couldn't update environment!");
-      toast.clearWaitingQueue();
-    }
-  };
-
-  const deleteEnvironment = async (mongoId) => {
-    const response = await apiService(environment(mongoId).delete);
-    if (response?.success) {
-      // setEnvironments(); response?.data
-    }
-  };
-
-  const toggleOpenEnvPopover = (event) => {
-    setOpenEnvPopover(openEnvPopover ? null : event?.currentTarget);
-  };
-
-  const handleCloseEnvPopover = () => {
-    if (openEnvPopover) {
-      editEnvironment(selectedEnv);
-    }
-    setOpenEnvPopover(null);
-  };
 
   const logout = () => {
     clearSession();
@@ -96,43 +45,6 @@ const Header = (props) => {
         )}
       </section>
       <section className={appStyles["app-header--right"]}>
-        {selectedEnv && (
-          <div className={appStyles["app-header__envs"]}>
-            <div className={appStyles["app-header__envs-dropdown"]}>
-              <ThemeAutocomplete
-                options={environments}
-                getOptionLabel={(option) => option?.envName || ""}
-                customStyle={{ width: "250px", padding: 0, color: "lightgrey" }}
-                renderInput={(params) => (
-                  <ThemeTextField
-                    {...params}
-                    InputLabelProps={{
-                      focused: false,
-                    }}
-                  />
-                )}
-                onChange={(e, selectedOption) => {
-                  setSelectedEnv(selectedOption);
-                }}
-                value={selectedEnv || ""}
-              />
-            </div>
-            <ClickAwayListener onClickAway={handleCloseEnvPopover}>
-              <div className={appStyles["app-header__edit-env"]}>
-                <ListAltIcon className={appStyles["app-header__edit-env__icon"]} onClick={toggleOpenEnvPopover} />
-                <EnvPopoverComponent
-                  openEnvPopover={openEnvPopover}
-                  handleCloseEnvPopover={handleCloseEnvPopover}
-                  selectedEnv={selectedEnv}
-                  setSelectedEnv={setSelectedEnv}
-                  selectedEnvOldData={selectedEnvOldData}
-                  setSelectedEnvOldData={setSelectedEnvOldData}
-                />
-              </div>
-            </ClickAwayListener>
-          </div>
-        )}
-
         <LogoutIcon className={appStyles["logout-btn"]} onClick={logout} />
       </section>
     </header>
--- src/pages/documentation/Documentation.jsx
+++ src/pages/documentation/Documentation.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { Tooltip } from "@material-ui/core";
+import { Tooltip, ClickAwayListener } from "@material-ui/core";
+import { toast } from "react-toastify";
+import { ListAlt as ListAltIcon } from "@material-ui/icons";
 
 // IMPORT USER-DEFINED COMPONENTS HERE
 import HeaderComponent from "components/header/Header";
@@ -10,13 +12,17 @@ import EndpointsWrapperComponent from "components/endpointsWrapper/EndpointsWrap
 import apiService from "apis/apiService";
 import { readme, schema, apisTree, environment } from "apis/urls";
 import { getUrlParams, sortArrayOfObjs } from "utils/functions";
-import { ThemeSwitch } from "utils/commonStyles/StyledComponents";
+import { ThemeSwitch, ThemeAutocomplete, ThemeTextField } from "utils/commonStyles/StyledComponents";
 import useGlobal from "redux/globalHook";
+import EnvPopoverComponent from "components/envPopover/EnvPopover";
 
 // IMPORT ASSETS HERE
 import appStyles from "./Documentation.module.scss";
 
-const Documentation = () => {
+const Documentation = (props) => {
+  // VARIABLES HERE
+  const serviceMID = getUrlParams()?.serviceMID;
+
   // HOOKS HERE
   const [selectedEnv, setSelectedEnv] = useState({});
   const [models, setModels] = useState([]);
@@ -25,6 +31,8 @@ const Documentation = () => {
   const [enableEditMode, setEnableEditMode] = useState(false);
   const [globalState] = useGlobal();
   const [environments, setEnvironments] = useState([]);
+  const [openEnvPopover, setOpenEnvPopover] = useState(null);
+  const [selectedEnvOldData, setSelectedEnvOldData] = useState(null);
 
   useEffect(() => {
     // fetchModels();
@@ -82,14 +90,45 @@ const Documentation = () => {
     setSortedApisTree(sortedApisTreeTemp);
   };
 
+  const createEnvironment = async (reqBody) => {
+    const response = await apiService(environment().post, { ...reqBody, serviceMID });
+    if (response?.success) {
+      // setEnvironments(); response?.data
+    }
+  };
+
+  const editEnvironment = async (updatedEnvironment) => {
+    const response = await apiService(environment(updatedEnvironment?._id).put, updatedEnvironment);
+    if (response?.success) {
+      setSelectedEnvOldData({});
+    } else {
+      setSelectedEnv(selectedEnvOldData);
+      toast.error("Couldn't update environment!");
+      toast.clearWaitingQueue();
+    }
+  };
+
+  const deleteEnvironment = async (mongoId) => {
+    const response = await apiService(environment(mongoId).delete);
+    if (response?.success) {
+      // setEnvironments(); response?.data
+    }
+  };
+
+  const toggleOpenEnvPopover = (event) => {
+    setOpenEnvPopover(openEnvPopover ? null : event?.currentTarget);
+  };
+
+  const handleCloseEnvPopover = () => {
+    if (openEnvPopover) {
+      editEnvironment(selectedEnv);
+    }
+    setOpenEnvPopover(null);
+  };
+
   return (
     <section className={appStyles["main-container"]}>
-      <HeaderComponent
-        environments={environments}
-        setEnvironments={setEnvironments}
-        selectedEnv={selectedEnv}
-        setSelectedEnv={setSelectedEnv}
-      />
+      <HeaderComponent />
       <section className={appStyles["content-cnt"]}>
         <section className={appStyles["table-of-contents"]}>
           <TableOfContentsComponent
@@ -131,6 +170,41 @@ const Documentation = () => {
             <div id="endpoints" className={appStyles.title}>
               <div>APIs</div>
               <div className={appStyles["title--right"]}>
+                <div className={appStyles.title__envs}>
+                  <div className={appStyles["title__envs-dropdown"]}>
+                    <ThemeAutocomplete
+                      options={environments}
+                      getOptionLabel={(option) => option?.envName || ""}
+                      customStyle={{ width: "250px", padding: 0 }}
+                      renderInput={(params) => (
+                        <ThemeTextField
+                          {...params}
+                          InputLabelProps={{
+                            focused: false,
+                          }}
+                        />
+                      )}
+                      onChange={(e, selectedOption) => {
+                        setSelectedEnv(selectedOption);
+                      }}
+                      value={selectedEnv || ""}
+                    />
+                  </div>
+                  <ClickAwayListener onClickAway={handleCloseEnvPopover}>
+                    <div className={appStyles["title__edit-env"]}>
+                      <ListAltIcon className={appStyles["title__edit-env__icon"]} onClick={toggleOpenEnvPopover} />
+                      <EnvPopoverComponent
+                        openEnvPopover={openEnvPopover}
+                        handleCloseEnvPopover={handleCloseEnvPopover}
+                        selectedEnv={selectedEnv}
+                        setSelectedEnv={setSelectedEnv}
+                        selectedEnvOldData={selectedEnvOldData}
+                        setSelectedEnvOldData={setSelectedEnvOldData}
+                      />
+                    </div>
+                  </ClickAwayListener>
+                </div>
+
                 {globalState?.loggedInUser?.editAccess?.includes?.(getUrlParams?.()?.serviceName) && (
                   <Tooltip title={enableEditMode ? "Edit Mode" : "View Mode"}>
                     <ThemeSwitch
