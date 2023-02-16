5b6dcce41c88691c0c7d9d8625d461ff99b7f014
Paras Agrawal <parasagrawal71@gmail.com>
2023-02-16 23:01:46 +0530

    Change SERVER URL

--- src/utils/constants/index.js
+++ src/utils/constants/index.js
@@ -2,7 +2,7 @@
  * URLs
  */
 export const SERVER_URL_LOCAL = "http://localhost:5001/api/v1";
-export const SERVER_URL_HEROKU = "https://api-documenter-server.herokuapp.com/api/v1";
+export const SERVER_URL_HEROKU = "https://api-documenter-server.onrender.com/api/v1";
 
 /*
  * REGEXs
