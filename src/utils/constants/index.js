2da5df2e9e8c853f78ceef4fa39f755cbba57ddc
Paras Agrawal <parasagrawal71@gmail.com>
2023-02-18 12:21:58 +0530

    Change SERVER_URL_HEROKU

--- src/utils/constants/index.js
+++ src/utils/constants/index.js
@@ -2,7 +2,7 @@
  * URLs
  */
 export const SERVER_URL_LOCAL = "http://localhost:5001/api/v1";
-export const SERVER_URL_HEROKU = "https://api-documenter-server.herokuapp.com/api/v1";
+export const SERVER_URL_HEROKU = "web-production-f265.up.railway.app";
 
 /*
  * REGEXs
